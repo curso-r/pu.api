ddb3113e6dbbc278901e6f3ee88dfb14b6717035
Julio Trecenti <julio.trecenti@gmail.com>
2017-10-04 03:08:42 -0300

    decryptr script

---
+++ R/captcha.R
@@ -0,0 +1,16 @@
+library(magrittr)
+library(decryptr)
+
+reticulate::py_available(TRUE)
+suppressMessages(suppressWarnings({
+  rfb_model <- decryptrModels::read_model('rfb')
+  trt_model <- decryptrModels::read_model('trt')
+  tjmg_model <- decryptrModels::read_model('tjmg')
+  esaj_model <- decryptrModels::read_model('esaj')
+}))
+
+#* @post /predict_tjmg
+predict_tjmg <- function(img) {
+  img_decoded <- base64enc::base64decode(img)
+  predict(tjmg_model, newdata = decryptr::prepare(img_decoded))
+}
